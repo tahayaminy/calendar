02998158f5efb2ad9f96e7cc2e325a5fc254bd87
Anonymous <ParsWebDesigners@gmail.com>
2022-02-06 06:35:05 +0330

    remove today from other month

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var date = new Date();
+var readableDate=new Date();
 var originMonth=date.getMonth();
 const QS=Q=>{return document.querySelector(Q)};
 var prevWeek=date.getDay();
-function calendar(){
+var uptodate=false;
+function calendar(uptodate){
     const Months=["January","February","March","April","May","June","July","August","September","October","November","December"];
     const getLastDay= new Date(date.getFullYear(), date.getMonth()+1, 0);
     const lastDay= getLastDay.getDate();
@@ -22,30 +24,42 @@ function calendar(){
         QS('.days').innerHTML+=`<div style="cursor:default;"></div>`;
     }
     for(let count=1;count<=lastDay;count++){
-        QS('.days').innerHTML+=`<div ${(count==date.getDate()) ? "class='today'": "" }>${count}</div>`;
+        if(uptodate){
+            QS('.days').innerHTML+=`<div ${(count==date.getDate()) ? "class='today'": "" }>${count}</div>`;
+        }else{
+            QS('.days').innerHTML+=`<div>${count}</div>`;
+        }
     }
     QS('.week').children[prevWeek].classList.remove('today');
-    QS('.week').children[date.getDay()].classList.add('today');
+    if(uptodate){QS('.week').children[date.getDay()].classList.add('today');}
     prevWeek=date.getDay();
 
 }
-calendar();
+calendar(true);
 
 function prevMonth(btn){
+    uptodate=false;
     date.setMonth((originMonth==0)? (0):(--originMonth));
     if(originMonth==0){
         btn.style=`opacity:.2;cursor:default;`;
     }    
-    QS('.fa-angle-left').style=`opacity:1;cursor:cursor;`;
+    if(readableDate.getMonth()==originMonth){
+        uptodate=true;
+    }
+    QS('.fa-angle-right').style=`opacity:1;cursor:cursor;`;
     QS('.days').innerHTML='';
-    calendar();
+    calendar(uptodate);
 }
 function nextMonth(btn){
+    uptodate=false;
     date.setMonth((originMonth==11)? (11):(++originMonth));
     if(originMonth==0){
         btn.style=`opacity=.2;cursor:default;`;
     }
+    if(readableDate.getMonth()==originMonth){
+        uptodate=true;
+    }
     QS('.fa-angle-left').style=`opacity:1;cursor:cursor;`;
     QS('.days').innerHTML='';
-    calendar();
+    calendar(uptodate);
 }
